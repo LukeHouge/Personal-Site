f4421f935923c168c2a1a237f2105e69893d0b96
Luke Houge <lukehouge@gmail.com>
2019-03-10 14:33:26 -0500

    updating message sending method

--- api/index.js
+++ api/index.js
@@ -23,14 +23,7 @@ http.createServer((request, response) => {
 function sendMail(message) {
     var stream = fs.createWriteStream("mailScript.sh"); //creates a write stream to mailScript.sh script file
     stream.once('open', function (fd) { //file descriptor so you will be able to close stream
-        stream.write("(\n"); //open script
-        stream.write("  echo To: lukehouge@gmail.com\n"); //sets recipient to the address
-        stream.write("  echo From: contact@lukehouge.com\n"); // sets the from address to contact@lukehouge.com
-        stream.write("  echo \"Content-Type: text/html; \"\n"); // says the email will be in HTML format
-        stream.write("  echo Subject: New Form Submission from lukehouge.com\n"); // sets subject of email
-        stream.write("  echo\n");
-        stream.write("  echo Message: " + message + "\n"); // points the the mail content file
-        stream.write(") | sendmail -t\n"); // closes the script, then sends the mail using PostFix
+        stream.write("   echo " + message + " | mail -s 'New Form Submission from lukehouge@gmail.com' lukehouge@gmail.com"); //sets recipient to the address
         stream.end(); //finishes writing the file
     });
 
